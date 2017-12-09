9ac064816c0959061bc502f960ccd31269396ee2
Clare <chblackburne@gmail.com>
2017-12-09 18:05:47 +0000

    word cloud functionality added

--- public/app.js
+++ public/app.js
@@ -1,8 +1,13 @@
 var app = function(){
 
-var url = "https://www.metaweather.com/api/location/21125/"
+// var url = "https://www.metaweather.com/api/location/21125/";
+var wordcontainer = document.querySelector("#word-cloud");
+var text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum erat ac justo sollicitudin, quis lacinia ligula fringilla. Pellentesque hendrerit, nisi vitae posuere condimentum, lectus urna accumsan libero, rutrum commodo mi lacus pretium erat. Phasellus pretium ultrices mi sed semper. Praesent ut tristique magna. Donec nisl tellus, sagittis ut tempus sit amet, consectetur eget erat. Sed ornare gravida lacinia. Curabitur iaculis metus purus, eget pretium est laoreet ut. Quisque tristique augue ac eros malesuada, vitae facilisis mauris sollicitudin. Mauris ac molestie nulla, vitae facilisis quam. Curabitur placerat ornare sem, in mattis purus posuere eget. Praesent non condimentum odio. Nunc aliquet, odio nec auctor congue, sapien justo dictum massa, nec fermentum massa sapien non tellus. Praesent luctus eros et nunc pretium hendrerit. In consequat et eros nec interdum. Ut neque dui, maximus id elit ac, consequat pretium tellus. Nullam vel accumsan lorem.';
+var lines = text.split(/[,\. ]+/g);
+var title = "wordcloud";
+var wordcloud = new WordCloud(wordcontainer, title, lines);
 makeRequest(url, requestComplete);
-};
+}
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
@@ -16,4 +21,5 @@ var requestComplete = function(){
   var jsonString = this.responseText;
   var forecast = JSON.parse(jsonString);
   populateDisplay(forecast);
-}
+};
+window.addEventListener('load', app);
